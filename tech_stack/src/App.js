a84ef7a9940db3fd30f6f612b09d55a01f71727a
Timothy Dillman <timothydillman@Timothys-iMac.knology.net>
2018-02-01 21:47:31 -0500

    add a simple header to get something on the screen

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Container, Content, Header } from 'native-base';
+import { Container, Content, Header, Body, Text } from 'native-base';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
@@ -11,7 +11,11 @@ export default class App extends Component {
             <Provider store={createStore(reducers)}>
                 <Container>
                     <Content>
-                        <Header />
+                        <Header>
+                            <Body>
+                                <Text>Tech Stack</Text>
+                            </Body>
+                        </Header>
                     </Content>
                 </Container>
             </Provider>
