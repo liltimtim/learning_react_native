4a718ba36a742742a4a35b9aba3d348c8b33f420
Timothy Dillman <timothydillman@Timothys-iMac.knology.net>
2018-02-01 21:44:19 -0500

    add native base package

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Container, Content, Header } from 'native-base';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
@@ -8,7 +9,11 @@ export default class App extends Component {
     render() {
         return (
             <Provider store={createStore(reducers)}>
-                <View />
+                <Container>
+                    <Content>
+                        <Header />
+                    </Content>
+                </Container>
             </Provider>
         );
     }
